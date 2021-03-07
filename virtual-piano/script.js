e8119737889acae454c050b41cf0a1abc240c5d8
frostwOw13 <frostwow13@yandex.ru>
2021-03-07 23:28:49 +0300

    feat: add playing sounds with mouse and keyboard

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,1 +1,31 @@
+const pianoKeys = document.querySelectorAll('.piano-key');
 
+// FUNCTION: Playing sounds
+const playAudio = function (src) {
+    const audio = new Audio();
+    audio.src = src;
+    audio.currentTime = 0;
+    audio.play();
+};
+
+// Playing sound when click mouse
+pianoKeys.forEach(key => {
+    key.addEventListener('click',(e) => {
+        if(e.target.classList.contains('piano-key')) {
+            const note = e.target.dataset.note;
+            const src = `assets/audio/${note}.mp3`;
+            playAudio(src);
+          }   
+    });
+});
+
+// Playing sound when key was pressed
+window.addEventListener('keydown', (e) => {
+    pianoKeys.forEach(key => {
+        if (`Key${key.dataset.letter}` === e.code) {
+            const note = key.dataset.note;
+            const src = `assets/audio/${note}.mp3`;
+            playAudio(src);
+        };
+    });
+});
