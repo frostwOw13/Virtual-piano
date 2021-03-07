1290b82d76f92e98c54b9da7ab06b9ca03470332
frostwOw13 <frostwow13@yandex.ru>
2021-03-08 01:15:53 +0300

    feat: add switch notes to letter

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -1,4 +1,6 @@
 const pianoKeys = document.querySelectorAll('.piano-key');
+const btnLetters = document.querySelector('.btn-letters');
+const btnNotes = document.querySelector('.btn-notes');
 
 // FUNCTION: Playing sounds
 const playAudio = function (src) {
@@ -8,10 +10,33 @@ const playAudio = function (src) {
     audio.play();
 };
 
+// Switch to letters
+btnLetters.addEventListener('click', (e) => {
+    pianoKeys.forEach(key => {
+        key.classList.add('letter');
+    });
+    btnNotes.classList.remove('btn-active')
+    btnLetters.classList.add('btn-active')
+});
+
+// Switch to notes
+btnNotes.addEventListener('click', (e) => {
+    pianoKeys.forEach(key => {
+        key.classList.remove('letter');
+    });
+    btnLetters.classList.remove('btn-active')
+    btnNotes.classList.add('btn-active')
+});
+
 // Playing sound when click mouse
 pianoKeys.forEach(key => {
-    key.addEventListener('click',(e) => {
+    key.addEventListener('mousedown',(e) => {
         if(e.target.classList.contains('piano-key')) {
+            key.classList.add('piano-key-active');
+            key.classList.remove('piano-key-active');
+            // setTimeout(() => {
+            //     key.classList.remove('piano-key-active');
+            // }, 100);
             const note = e.target.dataset.note;
             const src = `assets/audio/${note}.mp3`;
             playAudio(src);
@@ -23,6 +48,10 @@ pianoKeys.forEach(key => {
 window.addEventListener('keydown', (e) => {
     pianoKeys.forEach(key => {
         if (`Key${key.dataset.letter}` === e.code) {
+            key.classList.add('piano-key-active');
+            setTimeout(() => {
+                key.classList.remove('piano-key-active');
+            }, 100);
             const note = key.dataset.note;
             const src = `assets/audio/${note}.mp3`;
             playAudio(src);
