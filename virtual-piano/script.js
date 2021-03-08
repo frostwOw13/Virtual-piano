4dee3230224f9c1875b950aa2edac23fc61fbdef
frostwOw13 <frostwow13@yandex.ru>
2021-03-08 14:20:29 +0300

    feat: add play keys when mouseover

--- virtual-piano/script.js
+++ virtual-piano/script.js
@@ -28,20 +28,40 @@ btnNotes.addEventListener('click', (e) => {
     btnNotes.classList.add('btn-active')
 });
 
-// Playing sound when click mouse
+// Playing sound when click mouse and over the keys
+let isDown = true;
+
 pianoKeys.forEach(key => {
-    key.addEventListener('mousedown',(e) => {
-        if(e.target.classList.contains('piano-key')) {
-            key.classList.add('piano-key-active');
-            key.classList.remove('piano-key-active');
-            // setTimeout(() => {
-            //     key.classList.remove('piano-key-active');
-            // }, 100);
-            const note = e.target.dataset.note;
-            const src = `assets/audio/${note}.mp3`;
-            playAudio(src);
-          }   
+    key.addEventListener('mousedown',(event) => {
+        isDown = true;
+        if (isDown) {
+            if(event.target.classList.contains('piano-key')) {
+                key.classList.add('piano-key-active');
+                const note = event.target.dataset.note;
+                const src = `assets/audio/${note}.mp3`;
+                playAudio(src);
+            };
+        }
+        pianoKeys.forEach(key1 => {
+            key1.addEventListener('mouseover', e => {
+                if (isDown) {
+                    if(e.target.classList.contains('piano-key')) {
+                        key1.classList.add('piano-key-active');
+                        const note1 = e.target.dataset.note;
+                        const src1 = `assets/audio/${note1}.mp3`;
+                        playAudio(src1);
+                    };
+                }
+            });
+            key1.addEventListener('mouseout', e => {
+                key1.classList.remove('piano-key-active');
+            });
+        });
     });
+    window.addEventListener('mouseup', e => {
+        key.classList.remove('piano-key-active');
+        isDown = false;
+    })
 });
 
 // Playing sound when key was pressed
